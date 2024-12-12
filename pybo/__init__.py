b0cba2b4b6639868019fa7932f4a4acc782c086c
BigMountain87 <sbchoi129@gmail.com>
2024-12-12 17:27:16 +0900

    전체 내용 추가

--- pybo/__init__.py
+++ pybo/__init__.py
@@ -2,14 +2,18 @@ from flask import Flask
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 
-import config
+# config.py파일을 읽어옴
+import config #DB가 연결 되어 있음
 
+# DB 관련 객체를 생성
 db = SQLAlchemy()
 migrate = Migrate()
 
-
+# 서버의 본문
 def create_app():
+    # 플라스크 서버 객체 생성
     app = Flask(__name__)
+    # config 파일을 기반으로 사용
     app.config.from_object(config)
 
     # ORM -> DB와 연관이 됨
@@ -18,8 +22,12 @@ def create_app():
     # models.py 파일을 import 사용
     from . import models
 
-    # 블루프린트 -> 라우트와 관련
-    from .views import main_views
+    # 블루프린트 -> 라우터와 관련
+    # 주소와 관련한 처리를 모아서 처리
+    # main_views : "/" 기준의 페이지를 리턴
+    # question_views: "/question/" 하위 주소 관련
+    from .views import main_views, question_views
     app.register_blueprint(main_views.bp)
+    app.register_blueprint(question_views.bp)
 
     return app
--- pybo/views/main_views.py
+++ pybo/views/main_views.py
@@ -1,4 +1,8 @@
-from flask import Blueprint
+from flask import Blueprint, url_for
+from werkzeug.utils import redirect
+
+# models.py에 Question 클래스를 가져와서 사용
+from pybo.models import Question
 
 # 객체 bp
 bp = Blueprint('main', __name__, url_prefix='/')
@@ -7,10 +11,11 @@ bp = Blueprint('main', __name__, url_prefix='/')
 # 예) url_prefix ='/main'
 # localhost:5000/main
 
+@bp.route('/')
+def index():
+    return redirect(url_for('question._list'))
+
 @bp.route('/hello')
 def hello_pybo():
     return 'Hello, Pybo!'
 
-@bp.route('/')
-def index():
-    return 'Pybo index'
